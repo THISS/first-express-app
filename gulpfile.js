9eaafa282981f1349ab0aa0b038f781f8e7e80b7
Brenton Holswich <brenton@Brentons-MacBook-Pro.local>
2017-03-15 11:26:52 -0700

    updated the gulpfile to restart from gulp-watch

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,29 @@ const gulp = require('gulp');
 const nodemon = require('gulp-nodemon');
 const notify = require('gulp-notify');
 const livereload = require('gulp-livereload');
+const watch = require('gulp-watch');
 
 // Task
 gulp.task('default', function() {
-	// listen for changes
+	
+		// listen for changes
 	livereload.listen();
 	// configure nodemon
-	nodemon({
+	let nmon = nodemon({
 		// the script to run the app
 		script: 'express-server.js',
-		ext: 'js',
-		// watch: [
-		// 	"views/"
-		// ],
-		legacyWatch: true
-
+		ext: 'js'
+		// legacyWatch: true
 	}).on('restart', function(){
 		// when the app has restarted, run livereload.
 		gulp.src('express-server.js')
-			.pipe(livereload());
+		.pipe(livereload());
 		console.log("Server is restarting...");
-	})
+	});
+	gulp.watch(['/views/**/*', 'express-server.js'], () => {
+		// force a restart
+		nmon.emit('restart');
+	});
+	
+	
 })
